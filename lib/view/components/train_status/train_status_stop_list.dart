5d29b61c2a0d4818b29663ba423b33b64fdda19f
Samuele Besoli <samuele.besoli.sb@gmail.com>
2024-10-28 01:23:16 +0100

    chore: add timeline to train status page

--- lib/view/components/train_status/train_status_stop_list.dart
+++ lib/view/components/train_status/train_status_stop_list.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:timelines/timelines.dart';
 import 'package:treninoo/model/Stop.dart';
 import 'package:treninoo/view/components/train_status/train_status_stop_row.dart';
+import 'package:treninoo/view/style/colors/grey.dart';
+import 'package:treninoo/view/style/colors/primary.dart';
 
 import '../../../cubit/predicted_arrival.dart';
 
-class TrainStatusStopList extends StatelessWidget {
+class TrainStatusStopList extends StatefulWidget {
   const TrainStatusStopList({
     Key? key,
     this.stops,
@@ -18,18 +21,57 @@ class TrainStatusStopList extends StatelessWidget {
   final int delay;
 
   @override
+  State<TrainStatusStopList> createState() => _TrainStatusStopListState();
+}
+
+class _TrainStatusStopListState extends State<TrainStatusStopList> {
+  @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      physics: AlwaysScrollableScrollPhysics(),
-      itemCount: stops!.length,
-      itemBuilder: (context, index) {
-        return TrainStatusStopRow(
-          stop: stops![index],
-          current: stops![index].station.stationName == currentStop,
-          delay: delay,
-          predicted: context.watch<PredictedArrivalCubit>().state,
-        );
-      },
+    return Timeline.tileBuilder(
+      theme: TimelineThemeData(
+        nodePosition: 0,
+        nodeItemOverlap: true,
+        connectorTheme: ConnectorThemeData(
+          thickness: 14,
+        ),
+      ),
+      builder: TimelineTileBuilder.connected(
+        indicatorBuilder: (context, index) {
+          bool confirmed = widget.stops![index].confirmed;
+          return DotIndicator(
+            size: 14,
+            color: confirmed ? Colors.white : Grey.darker,
+            border: Border.all(
+              color: confirmed ? Primary.normal : Grey.normal,
+              width: 2.5,
+            ),
+          );
+        },
+        connectorBuilder: (context, index, connectorType) {
+          Color color = Grey.normal;
+
+          if (index > widget.stops!.length - 1) {
+            return SolidLineConnector(color: color);
+          }
+
+          bool confirmed = widget.stops![index + 1].confirmed;
+          color = confirmed ? Primary.normal : Grey.normal;
+
+          return SolidLineConnector(
+            color: color,
+          );
+        },
+        contentsBuilder: (context, index) {
+          return TrainStatusStopRow(
+            stop: widget.stops![index],
+            current:
+                widget.stops![index].station.stationName == widget.currentStop,
+            delay: widget.delay,
+            predicted: context.watch<PredictedArrivalCubit>().state,
+          );
+        },
+        itemCount: widget.stops!.length,
+      ),
     );
   }
 }
