4f50d2766c08eca6f89744a5ab3dca320a62159b
Samuele Besoli <samuele.besoli.sb@gmail.com>
2026-03-16 23:27:31 +0100

    Update iOS deployment target and remove deprecated plugins
    - Changed iOS deployment target from 12.0 to 13.0 in AppFrameworkInfo.plist, Flutter.podspec, and project.pbxproj.
    - Removed flutter_keyboard_visibility and pointer_interceptor_ios plugins from Podfile.lock and project.pbxproj.
    - Updated dependencies in pubspec.yaml and pubspec.lock to their latest versions.
    - Added LLDB integration scripts for better debugging experience.
    - Cleaned up generated_plugin_registrant.dart by removing references to flutter_keyboard_visibility_web.

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -7,7 +7,6 @@
 // ignore_for_file: depend_on_referenced_packages
 
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 
@@ -16,7 +15,6 @@ import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 // ignore: public_member_api_docs
 void registerPlugins(Registrar registrar) {
   FirebaseCoreWeb.registerWith(registrar);
-  FlutterKeyboardVisibilityPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
